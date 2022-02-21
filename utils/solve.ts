5f883414e041282d07531be55c0870dca371d26d
Raymond Cox <raymond@raymondjcox.com>
2022-02-20 20:17:51 -0500

    first

---
+++ utils/solve.ts
@@ -0,0 +1,79 @@
+function isValid(puzzle: any, row: any, col: any, num: any) {
+  for (let c = 0; c < puzzle[row].length; c++) {
+    if (c !== col && puzzle[row][c] === num) {
+      return false;
+    }
+  }
+
+  for (let r = 0; r < puzzle.length; r++) {
+    if (r !== row && puzzle[r][col] === num) {
+      return false;
+    }
+  }
+
+  const startRow = row - (row % 3);
+  const startCol = col - (col % 3);
+
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (startRow + i === row && startCol + j === col) {
+        continue;
+      }
+      if (puzzle[startRow + i]?.[startCol + j] === num) {
+        return false;
+      }
+    }
+  }
+
+  return true;
+}
+
+export function solvable(puzzle: any) {
+  for (let i = 0; i < puzzle.length; i++) {
+    for (let j = 0; j < puzzle[i].length; j++) {
+      if (puzzle[i][j] !== 0 && !isValid(puzzle, i, j, puzzle[i][j])) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+export function solve(puzzle: any) {
+  const deepCopy = puzzle.map((row: any) => {
+    return [...row];
+  });
+
+  solveRec(deepCopy);
+  return deepCopy;
+}
+
+function solveRec(puzzle: any, row = 0, col = 0): any {
+  if (col === puzzle[0].length && row === puzzle.length - 1) {
+    return true;
+  }
+
+  if (col === puzzle[row].length) {
+    row++;
+    col = 0;
+  }
+
+  // Skip for numbers we can't change
+  if (puzzle[row][col] > 0) {
+    return solveRec(puzzle, row, col + 1);
+  }
+
+  for (let num = 1; num < 10; num++) {
+    if (isValid(puzzle, row, col, num)) {
+      puzzle[row][col] = num;
+
+      if (solveRec(puzzle, row, col + 1)) {
+        return true;
+      }
+    }
+    // backtrack
+    puzzle[row][col] = 0;
+  }
+
+  return false;
+}
